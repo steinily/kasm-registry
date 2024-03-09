8d6880ef6909eb19afc2357ece092b10f68a7fdf
H. Istvan <66251121+steinily@users.noreply.github.com>
2024-03-09 20:21:27 +0100

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     name: 'Kasm Technologies',
     description: 'The official store for Kasm supported workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://steinily.github.io/',
+    listUrl: 'https://steinily.github.io/kasm-registry',
     contactUrl: 'https://github.com/steinily/kasm-registry/issues',
   },
   reactStrictMode: true,
